10bcc1571ebeef5996a98ae7e83720bc84a297a0
unknown <selinaadwoa1@gmail.com>
2024-12-13 11:24:06 -0800

    adding files commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function checkGender() {
+    let gender = prompt("What is your gender please");
+    if (gender=="female"){
+        alert(`You are a girl👧🏽`);
+
+    } else if (gender== "male") {
+
+        alert(`You are a boy👦🏽`);
+    }
+}
+checkGender()
